130cb35091fd41af7148a16f034552da35db6df3
Jason Weinzierl <weinzierljason@gmail.com>
2025-02-19 13:42:33 -0600

    chore(ci): use vite to build app

--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/index.html"],
-  plugins: [],
-};
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+});
