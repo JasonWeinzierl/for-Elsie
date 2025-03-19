feb3d4b51534cbe8264148221e03b538d0f548e4
Jason Weinzierl <weinzierljason@gmail.com>
2025-03-19 00:11:38 -0500

    feat: skeleton pages for schedule, registry, faq, party, with smooth scrolling and hash routing

--- src/main.js
+++ src/main.js
@@ -1,2 +1,61 @@
 // @ts-check
 import './styles.css';
+
+/** @type {number | null} */
+let timeout = null;
+/** @param {string} hash */
+function debouncePushState(hash) {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(() => {
+    if (window.location.hash !== hash) {
+      history.pushState({}, '', hash);
+    }
+    timeout = null;
+  }, 200);
+}
+
+const intersectionObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const hash = `#${entry.target.id}`;
+      debouncePushState(hash);
+    }
+  });
+}, {
+  // Relative to the layout (or could be null since this is effectively the viewport).
+  root: document.getElementById('layout'),
+  // Shrink the intersection bounds to a vertically centered horizontal line.
+  rootMargin: '-50% 0% -50% 0%',
+  // Use the default value.
+  threshold: 0,
+});
+
+// Navigation for these pages.
+[
+  'welcome',
+  'schedule',
+  'registry',
+  'faq',
+  'wedding-party',
+  'about',
+].forEach(id => {
+  const element = document.getElementById(id);
+  if (element) {
+    intersectionObserver.observe(element);
+  }
+});
+
+// Smooth scroll when navigating back.
+window.addEventListener('hashchange', e => {
+  const element = document.querySelector(window.location.hash);
+  if (element) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      // Center to avoid conflicting with the intersection observer.
+      block: 'center',
+    });
+  }
+  e.preventDefault();
+});
