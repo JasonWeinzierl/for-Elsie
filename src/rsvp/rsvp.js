e213663c7e7fcafa2471820e501f3a64b6f17030
Jason Weinzierl <weinzierljason@gmail.com>
2025-03-19 22:32:47 -0500

    feat: rsvp page

---
+++ src/rsvp/rsvp.js
@@ -0,0 +1,2 @@
+// @ts-check
+import '../styles.css';
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 // @ts-check
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = import.meta.dirname;
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src', 'index.html'),
+        // rsvp: resolve(__dirname, 'src', 'rsvp/index.html'),
+      }
+    },
   },
   plugins: [
     tailwindcss(),
